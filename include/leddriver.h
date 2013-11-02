2600bcc2c37af4da3e18acd0008d60fc7ff5f412
Anders Arnholm <Anders@Arnholm.se>
2013-11-02 13:20:26 +0100

    More tests

--- include/leddriver.h
+++ include/leddriver.h
@@ -7,6 +7,11 @@ class LedDriver {
     static const int ROW2PIN = 8;
     static const int ROW3PIN = 7;
     static const int ROW4PIN = 4;
+    static const int COL0PIN = 5;
+    static const int COL1PIN = 6;
+    static const int COL2PIN = 9;
+    static const int COL3PIN = 10;
+    static const int COL4PIN = 11;
     void init() {
       pinMode(ROW0PIN, OUTPUT);
       pinMode(ROW1PIN, OUTPUT);
@@ -16,6 +21,11 @@ class LedDriver {
     }
     void display() {
       digitalWrite(ROW0PIN, HIGH);
+      digitalWrite(COL0PIN, HIGH);
+      digitalWrite(COL1PIN, HIGH);
+      digitalWrite(COL2PIN, HIGH);
+      digitalWrite(COL3PIN, HIGH);
+      digitalWrite(COL4PIN, HIGH);
     }
 };
 
--- test/leddriver_test.cpp
+++ test/leddriver_test.cpp
@@ -2,24 +2,31 @@
 #include "gmock/gmock.h"
 #include "Arduino.h"
 
-class leddriver : public ::testing::Test {
+using ::testing::AnyNumber;
+using ::testing::AnyOf;
+using ::testing::_;
+
+class TestLedDriver : public ::testing::Test {
   public:
   ArduinoMock* arduinoMock;
   LedDriver ledDriver;
-  leddriver() : arduinoMock(arduinoMockInstance()) {
+  TestLedDriver() : arduinoMock(arduinoMockInstance()) {
   }
-  virtual ~leddriver() {
+  virtual ~TestLedDriver() {
     releaseArduinoMock();
   }
 };
 
-TEST_F(leddriver, FirstCallToDisplayShouldTurnOnRow0)
+TEST_F(TestLedDriver, FirstCallToDisplayShouldTurnOnRow0)
 {
   EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::ROW0PIN, HIGH)).Times(1);
+  EXPECT_CALL(*arduinoMock, digitalWrite(AnyOf(LedDriver::COL0PIN,
+          LedDriver::COL1PIN, LedDriver::COL2PIN, LedDriver::COL3PIN,
+          LedDriver::COL4PIN), _)).Times(AnyNumber());
   ledDriver.display();
 }
 
-TEST_F(leddriver, CallInitShouldSetRow0PinToOutput)
+TEST_F(TestLedDriver, CallInitShouldSetRow0PinToOutput)
 {
   EXPECT_CALL(*arduinoMock, pinMode(LedDriver::ROW0PIN, OUTPUT)).Times(1);
   EXPECT_CALL(*arduinoMock, pinMode(LedDriver::ROW1PIN, OUTPUT)).Times(1);
@@ -29,3 +36,24 @@ TEST_F(leddriver, CallInitShouldSetRow0PinToOutput)
   ledDriver.init();
 }
 
+TEST_F(TestLedDriver, DefaultShouldFirstColumBeOff)
+{
+  EXPECT_CALL(*arduinoMock, digitalWrite(AnyOf(LedDriver::ROW0PIN, LedDriver::ROW1PIN, LedDriver::ROW2PIN,
+          LedDriver::ROW3PIN, LedDriver::ROW4PIN), _)).Times(AnyNumber());
+  EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::COL0PIN, HIGH)).Times(1);
+  EXPECT_CALL(*arduinoMock, digitalWrite(AnyOf(LedDriver::COL1PIN, LedDriver::COL2PIN, LedDriver::COL3PIN,
+          LedDriver::COL4PIN), _)).Times(AnyNumber());
+  ledDriver.display();
+}
+
+TEST_F(TestLedDriver, DefaultShouldColumsBeOff)
+{
+  EXPECT_CALL(*arduinoMock, digitalWrite(AnyOf(LedDriver::ROW0PIN, LedDriver::ROW1PIN, LedDriver::ROW2PIN,
+          LedDriver::ROW3PIN, LedDriver::ROW4PIN), _)).Times(AnyNumber());
+  EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::COL0PIN, HIGH)).Times(1);
+  EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::COL1PIN, HIGH)).Times(1);
+  EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::COL2PIN, HIGH)).Times(1);
+  EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::COL3PIN, HIGH)).Times(1);
+  EXPECT_CALL(*arduinoMock, digitalWrite(LedDriver::COL4PIN, HIGH)).Times(1);
+  ledDriver.display();
+}
