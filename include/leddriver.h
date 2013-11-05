3ff3a9264df122b042fa2ba96b708681c97a78a9
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-11-05 08:24:57 +0100

    Test one refactor.
    Move the the magic int 13 into a constant in the test

--- include/leddriver.h
+++ include/leddriver.h
@@ -3,8 +3,10 @@
 
 class LEDDriver
 {
+	int m_row0pin;
 public:
+	LEDDriver(int row0pin) : m_row0pin(row0pin) {}
 	void display() {
-		digitalWrite(13, HIGH);
+		digitalWrite(m_row0pin, HIGH);
 	}
 };
--- test/leddriver_test.cpp
+++ test/leddriver_test.cpp
@@ -3,13 +3,15 @@
 #include "leddriver.h"
 #include "Arduino.h"
 
+static const int ROW0PIN = 13;
+
 TEST(TestLedDriver, FirstCallToDisplayCallShouldLightRow0)
 {
 	ArduinoMock *arduinoMock = arduinoMockInstance();
 
-	EXPECT_CALL(*arduinoMock, digitalWrite(13, HIGH));
+	EXPECT_CALL(*arduinoMock, digitalWrite(ROW0PIN, HIGH));
 
-	LEDDriver ledDriver;
+	LEDDriver ledDriver(ROW0PIN);
 	ledDriver.display();
 
 	releaseArduinoMock();
