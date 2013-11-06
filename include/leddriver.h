9b58edfccbedccd9d2d61fa594ccc46f49d1d141
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-11-06 08:53:00 +0100

    GREEN 8,
    The simplest implemenations might solve some extra stuff as well. Don't forget to clean the data in the constructor, if you don't you might get loots of regression errors.

--- include/leddriver.h
+++ include/leddriver.h
@@ -10,20 +10,28 @@ private:
 	int m_rows[NOROWS];
 	int m_colums[NOCOLUMS];
 	int m_row;
+	int m_data[NOROWS][NOCOLUMS];
 
 public:
 	LEDDriver(const int rows[NOROWS], const int colums[NOCOLUMS]) : m_row(-1) {
 		memcpy(m_colums, colums, sizeof(m_colums));
 		memcpy(m_rows, rows, sizeof(m_rows));
+		memset(m_data, 0, sizeof(m_data));
 	}
-	void show(const int* matrix) {}
+	void show(const int* matrix) {
+		memcpy(m_data, matrix, sizeof(m_data));
+	}
 	void display() {
 		if(m_row >= 0) {
 			digitalWrite(m_rows[m_row], LOW);
 		}
 		++m_row;
 		for(int i = 0 ; i < NOCOLUMS ; ++i) {
-			digitalWrite(m_colums[i], HIGH);
+			if(m_data[m_row][i]) {
+				digitalWrite(m_colums[i], LOW);
+			} else {
+				digitalWrite(m_colums[i], HIGH);
+			}
 		}
 		digitalWrite(m_rows[m_row], HIGH);
 	}
