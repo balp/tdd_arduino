60a49008498888701b896ea6934a6f56794887fa
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-11-07 10:28:08 +0100

    Green 10: The most simple solution on getting a dead matrix.

--- include/gameoflife.h
+++ include/gameoflife.h
@@ -3,7 +3,12 @@
 
 class GameOfLife 
 {
+private:
+	DisplayUpdater* m_driver;
 public:
-	GameOfLife(DisplayUpdater* driver, int* seed) {}
-	void tick() {}
+	GameOfLife(DisplayUpdater* driver, int* seed) : m_driver(driver) {}
+	void tick() {
+		int result[] = { 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0 };
+		m_driver->show(result); 
+	}
 };
