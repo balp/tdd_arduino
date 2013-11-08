8c8a615712a3056c507c24ad6d7ab02719e402e8
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-11-08 16:18:02 +0100

    GREEN 12: Added the calc noNeighbours function.

--- include/gameoflife.h
+++ include/gameoflife.h
@@ -6,15 +6,28 @@ class GameOfLife
 private:
 	DisplayUpdater* m_driver;
 	int m_previus[LEDDriver::NOROWS][LEDDriver::NOCOLUMS];
+	int noNeighbours(int x, int y) {
+		return m_previus[(x-1) % LEDDriver::NOROWS][(y-1)%LEDDriver::NOCOLUMS] 
+		+ m_previus[(x-1) % LEDDriver::NOROWS][y] 
+		+ m_previus[(x-1) % LEDDriver::NOROWS][(y+1)%LEDDriver::NOCOLUMS]
+		+ m_previus[x][(y-1)%LEDDriver::NOCOLUMS]
+		+ m_previus[x][(y+1)%LEDDriver::NOCOLUMS]
+		+ m_previus[(x+1) % LEDDriver::NOROWS][(y-1)%LEDDriver::NOCOLUMS]
+		+ m_previus[(x+1) % LEDDriver::NOROWS][y]
+		+ m_previus[(x+1) % LEDDriver::NOROWS][(y+1)%LEDDriver::NOCOLUMS];
+	}
 public:
 	GameOfLife(DisplayUpdater* driver, int* seed) : m_driver(driver) {
 		memcpy(m_previus, seed, sizeof(m_previus));
 	}
 	void tick() {
 		int result[] = { 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0 };
-		if( m_previus[2][1] == 1 && 
-			m_previus[2][3] == 1) {
-				result[2*LEDDriver::NOCOLUMS + 2] = 1;
+		for(int x = 0 ; x < LEDDriver::NOROWS; ++x) {
+			for(int y = 0; y < LEDDriver::NOCOLUMS; ++y) {
+				if( noNeighbours(x,y) == 2 || noNeighbours(x,y) == 3) {
+					result[x*LEDDriver::NOCOLUMS + y] = 1;
+				}
+			}
 		}
 		m_driver->show(result); 
 	}
