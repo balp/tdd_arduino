655ff7564912a5c2d46bca029599a2f2b8da1918
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-11-08 16:07:59 +0100

    GREEN 11: Test the positions atound just needed.

--- include/gameoflife.h
+++ include/gameoflife.h
@@ -5,10 +5,17 @@ class GameOfLife
 {
 private:
 	DisplayUpdater* m_driver;
+	int m_previus[LEDDriver::NOROWS][LEDDriver::NOCOLUMS];
 public:
-	GameOfLife(DisplayUpdater* driver, int* seed) : m_driver(driver) {}
+	GameOfLife(DisplayUpdater* driver, int* seed) : m_driver(driver) {
+		memcpy(m_previus, seed, sizeof(m_previus));
+	}
 	void tick() {
 		int result[] = { 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0 };
+		if( m_previus[2][1] == 1 && 
+			m_previus[2][3] == 1) {
+				result[2*LEDDriver::NOCOLUMS + 2] = 1;
+		}
 		m_driver->show(result); 
 	}
 };
