dd5d5f90404db578acef2399f685aa8b32816f93
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-11-08 16:47:03 +0100

    RED 15: Expect the test to run over generations
    Also here we show a matcher that display how two boards differ. using the hidden *result_listener

--- test/gameoflife_test.cpp
+++ test/gameoflife_test.cpp
@@ -6,11 +6,33 @@
 #include <gmock/gmock.h>
 #include "Arduino.h"
 
+using ::testing::_;
+using ::testing::Mock;
+
 MATCHER_P(CenterPixelIs, value, "")
 { 
 	return arg[(LEDDriver::NOROWS/2)*LEDDriver::NOCOLUMS
 		+ (LEDDriver::NOCOLUMS/2)] == value;
 }
+MATCHER_P(GameBoardIs, value, "")
+{ 
+	if( memcmp(arg, value, sizeof(int) * 25) == 0 ) {
+		return true;
+	}
+	*result_listener << " {" ;
+	for(int i = 0 ; i < 25 ; ++i) {
+		*result_listener << (i ? "," : "") << arg[i];
+	}
+	*result_listener << "} != {" ;
+	for(int i = 0 ; i < 25 ; ++i) {
+		*result_listener << (i ? "," : "") << value[i];
+	}
+	*result_listener << "}" ;
+	
+	return false;
+}
+
+
 
 class TestGameOfLife : public ::testing::Test {
 protected:
@@ -54,5 +76,16 @@ TEST_F(TestGameOfLife, ADeadCellWithThreeNeighboursIsGetsBorn)
 	int startseed[] = {0,0,0,0,0, 0,0,1,0,0, 0,1,0,1,0, 0,0,0,0,0, 0,0,0,0,0};
 	EXPECT_CALL(mockdriver, show(CenterPixelIs(1))).Times(1);
 	GameOfLife gameoflife(&mockdriver, startseed);
+	gameoflife.tick();
+}
+
+TEST_F(TestGameOfLife, TheGameContinuesOnLastGeneration)
+{
+	int startseed[] = {0,0,0,0,0, 0,0,0,0,0, 0,1,1,1,0, 0,0,0,0,0, 0,0,0,0,0};
+	EXPECT_CALL(mockdriver, show(_)).Times(1);
+	GameOfLife gameoflife(&mockdriver, startseed);
+	gameoflife.tick();
+	Mock::VerifyAndClearExpectations(&mockdriver);
+	EXPECT_CALL(mockdriver, show(GameBoardIs(startseed))).Times(1);
 	gameoflife.tick();
 }
